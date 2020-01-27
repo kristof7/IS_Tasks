3eb9f3c0100f3fd7dd51f801edd09def10aa6e69
Jarek Szczepanik <ten.ancymon@gmail.com>
2020-01-27 18:45:00 +0100

    Zadanie A

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,20 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.zadanie1.Zadanie_A;
+
+import java.util.Scanner;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println("JJDZ8 - homeworks!");
+
+        /* Uruchomienie zadania 1A */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź ciąg znaków, a ja zwrócę pierwszą występującą LITERĘ w tym ciągu:");
+        String word = scanner.nextLine();
+        System.out.println(word + " - " + Zadanie_A.returnFirstLetterFromScanner(word));
+        scanner.close();
+
+
     }
 }
