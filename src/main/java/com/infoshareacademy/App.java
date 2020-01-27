dd223c03133cf689183be00d7cf58ff18f0735d7
Kamila Formela <kamilakformela@gmail.com>
2020-01-27 20:49:31 +0100

    zadanie B

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -4,7 +4,27 @@ import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) {
-      /*  System.out.println("Exercise A");
+
+        //ExerciseB
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        System.out.println("Please enter first number:");
+        calculator.num1 = scanner.nextDouble();
+
+        System.out.println("Please enter second number:");
+        calculator.num2 = scanner.nextDouble();
+
+        System.out.println("What action do you perform: +,-,/,*?");
+        Scanner scanner1 = new Scanner(System.in);
+        calculator.sign = scanner1.nextLine();
+
+
+        double result = calculator.calculate(calculator.num1,calculator.num2,calculator.sign);
+        System.out.println(calculator.num1 + calculator.sign + calculator.num2 + "="+ result);
+
+
+        /*  System.out.println("Exercise A");
 
         ExerciseA exerciseA = new ExerciseA();
 
---
+++ src/main/java/com/infoshareacademy/Calculator.java
@@ -0,0 +1,28 @@
+package com.infoshareacademy;
+
+public class Calculator {
+    double num1;
+    double num2;
+    String  sign;
+
+    public double calculate(double num1, double num2, String sign) {
+        switch (sign) {
+            case "+": {
+                return this.num1 + this.num2;
+            }
+            case "-": {
+                return this.num1 - this.num2;
+            }
+            case "*": {
+                return this.num1 * this.num2;
+            }
+            case "/": {
+                return this.num1 / this.num2;
+            }
+        }
+        return 0.0;
+
+    }
+
+
+}
