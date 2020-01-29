8cc016b3bbc36c5c4a27d1c473badf573357bb41
Dominik Rynkiewicz <dominik.rynkiewicz@gmail.com>
2020-01-29 21:39:59 +0100

    zadanie 1 - ver. 1

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,19 @@
 package com.infoshareacademy;
 
+import java.util.Scanner;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println("JJDZ8 - homeworks!");
+
+        printChar("");
+    }
+
+
+    public static char printChar(String name) {
+        System.out.println("Podaj jakiś wyraz");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        System.out.println(name + " - " + name.charAt(0));
+        return name.charAt(0);
     }
 }
