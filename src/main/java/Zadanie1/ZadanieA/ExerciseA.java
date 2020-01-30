67f023fd61f7b661364451f9b22682731e6ff78f
Krzysztof K <krzysztofk7777@gmail.com>
2020-01-30 23:19:48 +0100

    Exercise A

---
+++ src/main/java/Zadanie1/ZadanieA/ExerciseA.java
@@ -0,0 +1,9 @@
+package Zadanie1.ZadanieA;
+
+public class ExerciseA {
+
+    public static char stringToChar(String word) {
+        char actualValue = word.charAt(0);
+        return actualValue;
+    }
+}
--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,18 @@
 package com.infoshareacademy;
 
+import Zadanie1.ZadanieA.ExerciseA;
+
+import java.util.Scanner;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println("JJDZ8 - homeworks!");
+
+//--------------ZADANIE A--------------------------
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word.., program will display first letter of the world:");
+        String charWord = scanner.nextLine();
+        System.out.println(charWord+" - "+ExerciseA.stringToChar(charWord));
+//--------------------------------------------------
+
     }
 }
