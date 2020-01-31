25f94b6e4c90334eff00f1394a4556c7dc8c6dbb
Dominik Rynkiewicz <dominik.rynkiewicz@gmail.com>
2020-01-31 10:15:20 +0100

    Zadanie 1B - poprawione

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,13 +1,48 @@
 package com.infoshareacademy;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        printChar("cokolwiek");
-    }
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Podaj dwie dowolne liczby?");
+            double number1 = scanner.nextDouble();
+            double number2 = scanner.nextDouble();
 
 
-    public static char printChar(String name) {
-        System.out.println(name + " - " + name.charAt(0));
-        return name.charAt(0);
+            System.out.println("Twoje liczby to: " + number1 + " " + number2);
+
+            System.out.println("Podaj jakie działania arytmetyczne z podanych w nawiasie chcesz wykonać " +
+                    "na tych liczbach (+, -, *, /)?");
+
+            char ch;
+            ch = (char) System.in.read();
+
+            switch (ch) {
+                case '+':
+                    System.out.println("Wynik Twojego działania to " + (number1 + number2));
+                    break;
+                case '-':
+                    System.out.println("Wynik Twojego działania to " + (number1 - number2));
+                    break;
+                case '*':
+                    System.out.println("Wynik Twojego działania to " + number1 * number2);
+                    break;
+                case '/':
+                    System.out.println("Wynik Twojego działania to " + number1 / number2);
+                    break;
+                default:
+                    System.out.println("Nie ma takiego działania.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Nieprawidłowa liczba.");
+        }
     }
 }
+
+
+
