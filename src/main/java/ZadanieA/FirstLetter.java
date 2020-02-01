d92a4051364c29b6b7b0513e776c7bad4ca15db2
Krzysztof Wojcieszak <k.j.wojcieszak@gmail.com>
2020-02-01 10:41:40 +0100

    zadanie A

---
+++ src/main/java/ZadanieA/FirstLetter.java
@@ -0,0 +1,10 @@
+package ZadanieA;
+
+public class FirstLetter {
+
+    public static void main(String[] args) {
+        String name="Plecak";
+        System.out.println(name + " - "+Method.firstLetterOfTheWord(name));
+    }
+
+}
---
+++ src/main/java/ZadanieA/Method.java
@@ -0,0 +1,10 @@
+package ZadanieA;
+
+public class Method {
+
+    public static char firstLetterOfTheWord(String name){
+        char[] charArray = name.toCharArray();
+        return charArray[0];
+    }
+
+}
