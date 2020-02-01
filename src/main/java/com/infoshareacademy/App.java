37f50d5e317fd47610b48d8951c46f3dc1fe5393
Pawel Madrak <madrak6@gmail.com>
2020-02-01 12:24:43 +0100

    Zadanie A

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,33 @@
 package com.infoshareacademy;
 
+import java.util.Scanner;
+
 public class App {
-    public static void main( String[] args ) {
-        System.out.println("JJDZ8 - homeworks!");
+
+    static char litera;
+    static String wyraz;
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("cw1");
+        System.out.println("podaj napis:");
+
+        wyraz = scanner.nextLine();
+        System.out.println(wyraz+" - "+metoda(wyraz));
     }
+
+    public static char metoda(String a) {
+
+        for (int i = 0; i < a.length(); i++) {
+            char[] listaliter = a.toCharArray();
+            litera = listaliter[0];
+        }
+
+        return litera;
+
+    }
+
 }
