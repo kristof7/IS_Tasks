aca705c26e83b489badb0d6b146f6262b7489137
dzaorski <59510381+dzaorski@users.noreply.github.com>
2020-02-02 22:24:21 +0100

    Zadanie A
    Poprawne polozenie plikow.

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,12 @@
-package com.infoshareacademy;
+package main.java.com.infoshareacademy;
 
 public class App {
     public static void main( String[] args ) {
         System.out.println("JJDZ8 - homeworks!");
+
+        //Zadanie A
+        System.out.println("Zadanie A");
+        FirstLetter firstletter = new FirstLetter();
+        firstletter.firstLetter();
     }
 }
