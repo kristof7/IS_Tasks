d955b90197335853eb3559e9574e17f741c2f133
Dominika <burakiewicz.dominika@gmail.com>
2020-02-02 21:13:55 +0100

    1a

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -3,30 +3,15 @@ package com.infoshareacademy;
 import java.util.Scanner;
 
 public class App {
- //@Homework3
- public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s + " - " + myMethod(s));
 
-     Card card1 = new Card();
-     Card card2 = new Card();
-
-     card1.setRank(Ranks.ACE);
-     card1.setSuit(Suits.CLUBS);
-     System.out.println(card1.getDescription(card1.getSuit(), card1.getRank()));
-
-     card2.setRank((Ranks.JACK));
-     card2.setSuit(Suits.SPADES);
-     System.out.println(card2.getDescription(card2.getSuit(),card2.getRank()));
- }
-    public enum Suits {
-        CLUBS,
-        DIAMONDS,
-        HEARTS,
-        SPADES;
     }
-    public enum Ranks {
-        ACE,
-        KING,
-        QUEEN,
-        JACK;
+    public static char myMethod (String a) {
+        char b = a.charAt(0);
+        return b;
     }
 }
+
