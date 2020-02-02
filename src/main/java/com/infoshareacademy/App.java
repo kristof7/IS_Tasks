8a3b3d021527198c474c7b18fafe7ae23c816574
Dawid Jurkiewicz <dawid.k.jurkiewicz@gmail.com>
2020-02-02 13:57:54 +0100

    Drugie zadanie

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -6,25 +6,44 @@ public class App {
     public static void main( String[] args ) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Napisz cos! ");
-        String s = scanner.nextLine();
-        char sReturned = returnFirstCharacter(s);
-        System.out.println(s + " - "+sReturned);
+        Scanner scanner1 = new Scanner(System.in);
 
-        String a = "Test";
-        String b = "2. Test";
-        String c = ".3 Test";
-        String d = "  cztery";
 
-        char aReturned = returnFirstCharacter(a);
-        char bReturned = returnFirstCharacter(b);
-        char cReturned = returnFirstCharacter(c);
-        char dReturned = returnFirstCharacter(d);
+        System.out.println("Prosty kalkulatorek, który dodaj odejmuje mnoży i dzieli!");
+        System.out.println("Podaj pierwszą liczbę: ");
+        double a = scanner.nextDouble();
+        System.out.println("Podaj drugą liczbę: ");
+        double b = scanner.nextDouble();
+        System.out.println("Jaką operacje chcesz wykonać? Wpisz :");
+        System.out.println("+ dodawnie");
+        System.out.println("- odejmowanie");
+        System.out.println("* mnożenie");
+        System.out.println("/ dzielenie");
+        String operacja = scanner1.nextLine();
 
-        System.out.println(a + " - " + aReturned);
-        System.out.println(b + " - " + bReturned);
-        System.out.println(c + " - " + cReturned);
-        System.out.println(d + " - " + dReturned);
+
+        if (operacja.equals("+")){
+            double result = a + b;
+            System.out.println("wynik działania "+ a + operacja + b +" = " + result);
+        }
+        else if (operacja.equals("-")) {
+            double result =  a - b;
+            System.out.println("wynik działania " + a + operacja + b + " = " + result);
+        }
+        else if (operacja.equals("*")){
+            double result = a * b;
+            System.out.println("wynik działania " + a + operacja + b + " = " + result);
+        }
+        else if (operacja.equals("/")){
+            double result = a / b;
+            System.out.println("wynik działania " + a + operacja + b + " = " + result);
+        }
+        else {
+            System.out.println("Błędny znak operacji");
+        }
+
+
+
 
     }
 
