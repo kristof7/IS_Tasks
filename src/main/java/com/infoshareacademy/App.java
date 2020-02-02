c0de860c667f67a66d08a821dbeaa9705f2f09a2
Dawid Jurkiewicz <dawid.k.jurkiewicz@gmail.com>
2020-02-02 13:25:33 +0100

    Pierwsze zadanie

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,37 @@
 package com.infoshareacademy;
 
+import java.util.Scanner;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println("JJDZ8 - homeworks!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Napisz cos! ");
+        String s = scanner.nextLine();
+        char sReturned = returnFirstCharacter(s);
+        System.out.println(s + " - "+sReturned);
+
+        String a = "Test";
+        String b = "2. Test";
+        String c = ".3 Test";
+        String d = "  cztery";
+
+        char aReturned = returnFirstCharacter(a);
+        char bReturned = returnFirstCharacter(b);
+        char cReturned = returnFirstCharacter(c);
+        char dReturned = returnFirstCharacter(d);
+
+        System.out.println(a + " - " + aReturned);
+        System.out.println(b + " - " + bReturned);
+        System.out.println(c + " - " + cReturned);
+        System.out.println(d + " - " + dReturned);
+
+    }
+
+public static char returnFirstCharacter(String input) {
+
+        char firstCharacter = input.charAt(0);
+
+        return firstCharacter;
     }
 }
