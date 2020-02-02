9c4884997763a40d23aee774a52f0159d8b5e849
Paweł <pawel.molesta@gmail.com>
2020-02-02 11:32:31 +0100

    Zadanie A

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,21 @@
 package com.infoshareacademy;
 
+import java.util.Scanner;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println("JJDZ8 - homeworks!");
+
+
+        System.out.println("Zadanie 1 - A : ");
+
+        ExerciseA zadanie1A = new ExerciseA();
+
+        System.out.println("Podaj jakis wyraz : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        char letter = zadanie1A.firstChar(input);
+
+        System.out.println(input + " - " + letter);
     }
 }
---
+++ src/main/java/com/infoshareacademy/ExerciseA.java
@@ -0,0 +1,9 @@
+package com.infoshareacademy;
+
+public class ExerciseA {
+
+    public char firstChar(String input) {
+        return input.charAt(0);
+    }
+
+}
