38e1dd9cbfd77675ec070fa1b91290a5f2b39d4c
Dominika <burakiewicz.dominika@gmail.com>
2020-02-02 17:58:52 +0100

    2_DominikaBurakiewicz

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -3,14 +3,33 @@ package com.infoshareacademy;
 import java.util.Scanner;
 
 public class App {
+ //@Homework2
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        System.out.println(s + " - " + myMethod(s));
-    }
+        System.out.println("Podaj pierwsza liczbe: ");
+        int a = scanner.nextInt();
+        System.out.println("Podaj druga liczbe: ");
+        int b = scanner.nextInt();
+        Scanner scanner1 = new Scanner((System.in));
+        System.out.println("Podaj symbol dzialania(+,-,*,/");
+        String z = scanner1.nextLine();
 
-    public static char myMethod(String a) {
-        char b = a.charAt(0);
-        return b;
+        int sum;
+        if(z.equals("+")){
+          sum = a + b;
+          System.out.println("Wynik dzialania " + a + " " + z + " " + b + " = " + sum);
+        }
+        if (z.equals("-")){
+            sum = a - b;
+            System.out.println("Wynik dzialania " + a + " " + z + " " + b + " = " + sum);
+        }
+        if (z.equals("*")){
+            sum = a * b;
+            System.out.println("Wynik dzialania " + a + " " + z + " " + b + " = " + sum);
+        }
+        if (z.equals("/")){
+            sum = a/b;
+            System.out.println("Wynik dzialania " + a + " " + z + " " + b + " = " + sum);
+        }
     }
 }
