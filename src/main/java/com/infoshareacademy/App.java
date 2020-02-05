aaa21e91ec6435205c2fe778871ce058d443ccfb
Grzegorz Nagórski <sparki_21@tlen.pl>
2020-02-05 16:50:46 +0100

    zadanie 1

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,17 @@
 package com.infoshareacademy;
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         System.out.println("JJDZ8 - homeworks!");
-    }
+
+        Person person1 = new Person();
+        person1.name = "Ania";
+        Person person2 = new Person();
+        person2.name = "Kasia";
+        person1.printChar();
+        person2.printChar();
+        }
+
 }
+
+
---
+++ src/main/java/com/infoshareacademy/Person.java
@@ -0,0 +1,10 @@
+package com.infoshareacademy;
+
+public class Person {
+String name;
+
+    void printChar() {
+        System.out.println(name + " - " + name.charAt(0));
+
+    }
+}
