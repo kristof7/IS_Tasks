e18505454b79f50b0ca830b507909bfb397b1321
tomlisow <tomasz.lisowski@protonmail.ch>
2020-02-07 14:17:38 +0100

    TaskB

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,15 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.tasks.TaskA;
+import com.infoshareacademy.tasks.TaskB;
+
+import java.util.Scanner;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println("JJDZ8 - homeworks!");
+
+
     }
+
+
 }
