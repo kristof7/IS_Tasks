3b9dea7bc9308580d0a6610e4e1d37b6a6d14598
Grzegorz Nagórski <sparki_21@tlen.pl>
2020-02-11 22:57:21 +0100

    Ex2 ver 2.0

--- Ex2/src/main/java/infoshareacademy/Lotto.java
+++ Ex2/src/main/java/infoshareacademy/Lotto.java
@@ -1,8 +1,6 @@
 package infoshareacademy;
 
-import java.util.InputMismatchException;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 import static java.lang.System.out;
 
@@ -10,20 +8,30 @@ class Lotto {
 
 
     static void randomLotto() {
-            Scanner scanner1 = new Scanner(System.in);
-            out.println("Ile liczb chcesz uzyskać??");
-            try {
-                int i = scanner1.nextInt();
-                Scanner scanner2 = new Scanner(System.in);
-                out.println("podaj zakres");
-                int l = scanner2.nextInt();
-                Random r = new Random();
-                for (int j = 0; j < i; j++) out.println(r.nextInt(l));
-            }
-            catch (InputMismatchException r) {
-                System.out.println("podany znak nie jest liczbą");
-            }
+        Scanner scanner1 = new Scanner(System.in);
+        out.println("Ile liczb chcesz uzyskać??");
+        try {
+            int i = scanner1.nextInt();
+            Scanner scanner2 = new Scanner(System.in);
+            out.println("podaj zakres");
+            int l = scanner2.nextInt();
 
+            Set<Integer> zbior = new HashSet<Integer>();
+            Random r = new Random();
+
+            for (int j = 0; j < i; j++)
+                for (int k = 0; k<i; k++)
+                    zbior.add(r.nextInt(l));
+
+            for (Integer liczbaLosowa : zbior)
+
+            {
+                System.out.println(liczbaLosowa);
+            }
         }
-    }
+        catch (InputMismatchException r) {
+            System.out.println("podany znak nie jest liczbą");
+        }
 
+    }
+}
