d5cfa5ee9fe0974aaa640ea0e46bad2f10dc6368
Dominik Rynkiewicz <dominik.rynkiewicz@gmail.com>
2020-02-12 18:39:40 +0100

    Ex2 - ver. 2 - sorted numbers

--- Ex2/src/main/java/infoshareacademy/Lotto.java
+++ Ex2/src/main/java/infoshareacademy/Lotto.java
@@ -8,27 +8,31 @@ public class Lotto {
     public static void start() {
         try {
             Scanner scan = new Scanner(System.in);
-            System.out.println("Podaj końcową liczbę zakresu z jakiego będziesz losował (od 1 do...) ?");
+            System.out.println("Podaj końcową liczbę zakresu z jakiego będziesz losował (od 1 do...)?");
             int rangeOfNumbers = scan.nextInt();
             System.out.println("Zakres liczb wybranych do losowania to: 1 - " + rangeOfNumbers);
-
             System.out.println("Podaj ilość liczb do losowania z wybranego zakresu?");
             int numberOfDraws = scan.nextInt();
-            ArrayList numbers = new ArrayList();
+
+            ArrayList<Integer> numbers = new ArrayList<>();
+            ArrayList<Integer> randomNumbers = new ArrayList<>();
+
             for (int i = 0; i < rangeOfNumbers; i++) {
                 numbers.add(i + 1);
+                Collections.shuffle(numbers);
             }
-            Collections.shuffle(numbers);
             System.out.println("Chcesz wylosować " + numberOfDraws + " liczb(y). Zwalniam mechanizm maszyny losującej..." +
                     " Oto wynik losowania:");
             for (int j = 0; j < numberOfDraws; j++) {
-                System.out.println(numbers.get(j));
+                randomNumbers.add(numbers.get(j));
+                Collections.sort(randomNumbers);
             }
-
-
+            for (Integer randomNumber: randomNumbers) {
+                System.out.println(randomNumber);
+            }
         } catch (Exception e) {
-            System.out.println("Coś poszło nie tak. Spróbuj ponownie odpalić maszynę losującą.");
+            System.out.println("Coś poszło nie tak. Spróbuj ponownie odpalić maszynę losującą i postępować zgodnie " +
+                    "z wyświetlającymi się instrukcjami.");
         }
     }
 }
-
