601c3abef0df93b842288b9be4182e10c431bfbd
Krzysztof Kampa <krzysztofk7777@gmail.com>
2020-02-14 22:12:34 +0100

    Ex2 - TaskA/display interval added

--- Ex2/src/main/java/infoshareacademy/App.java
+++ Ex2/src/main/java/infoshareacademy/App.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class App {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         Lotto lotto = new Lotto();
         lotto.Start();
