443fa34e9812258e54a614f061e40cc94b830fc6
Krzysztof Kampa <krzysztofk7777@gmail.com>
2020-02-14 20:58:26 +0100

    Ex2 - TaskA/sorted list and writer added

--- Ex2/src/main/java/infoshareacademy/App.java
+++ Ex2/src/main/java/infoshareacademy/App.java
@@ -1,10 +1,11 @@
 package infoshareacademy;
 
+import java.io.IOException;
 import java.util.*;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Lotto lotto = new Lotto();
         lotto.Start();
