ff006c0a6b2ff7cd14844f493ebf323b42afb624
Kamila Formela <kamilakformela@gmail.com>
2020-02-18 21:53:42 +0100

    update

--- Ex3/src/main/java/infoshareacademy/PizzeriaApp.java
+++ Ex3/src/main/java/infoshareacademy/PizzeriaApp.java
@@ -14,7 +14,7 @@ public class PizzeriaApp {
         orderService.createOrder(foods,
                 new Client("Adam", new Address("Pogodna", "Gdańsk"), "7406938694"));
         orderService.printOrder();
-        System.out.println("Total cost " + orderService.calculateTotalCost(foods));
+        System.out.println("Total cost " + orderService.calculateTotalCost(foods)+ "\n\n");
 
         OrderService orderService1 = new OrderService();
         Foods foods1 = new Foods(new Pizza(1, PizzaSize.SMALL, PizzaType.HAWAII), new Pizza(1, PizzaSize.MEDIUM, PizzaType.CAPRICOSA),
@@ -22,7 +22,7 @@ public class PizzeriaApp {
         orderService1.createOrder(foods1,
                 new Client("Anna", new Address("Długa 18", "Gdańsk"), "74069390994"));
         orderService1.printOrder();
-        System.out.println("Total cost " + orderService1.calculateTotalCost(foods1));
+        System.out.println("Total cost " + orderService1.calculateTotalCost(foods1) + "\n\n");
 
         OrderService orderService2 = new OrderService();
         Foods foods2 = new Foods(new Kebab(1, KebabType.HAMBURGER),
@@ -30,6 +30,6 @@ public class PizzeriaApp {
         orderService2.createOrder(foods2,
                 new Client("Paweł", new Address("Grunwaldzka", "Gdańsk"), "2339938694"));
         orderService2.printOrder();
-        System.out.println("Total cost " + orderService2.calculateTotalCost(foods2));
+        System.out.println("Total cost " + orderService2.calculateTotalCost(foods2) + "\n\n");
     }
 }
