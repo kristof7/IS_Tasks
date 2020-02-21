d226d40971b1f6056ffda6dc9f2598dc72e5e90b
Dominik Rynkiewicz <dominik.rynkiewicz@gmail.com>
2020-02-21 20:44:22 +0100

    Ex3 - ver. 3

--- Ex3/src/main/java/infoshareacademy/PizzeriaApp.java
+++ Ex3/src/main/java/infoshareacademy/PizzeriaApp.java
@@ -1,5 +1,8 @@
 package infoshareacademy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PizzeriaApp {
     public static void main(String[] args) {
 
@@ -8,26 +11,31 @@ public class PizzeriaApp {
         Client client3 = new Client("Paweł", "ul. Grunwaldzka 472", "tel. 321 654 987");
 
         Order order1 = new Order();
-        System.out.println("ORDER 1:");
         order1.setClient(client1);
-        System.out.print(order1.getClient().getClientDetails() + ", ");
-        System.out.print(order1.getPizza().addMeal(Meal.Size.LARGE, Meal.Type.PEPPERONI) + ", ");
-        System.out.println(order1.getBeverage().addBeverage("JUICE", "TWO\n"));
+        String orderOne = "ORDER 1: " + order1.getClient().getClientDetails() + ", " + order1.getPizza().addMeal
+        (Meal.Size.LARGE, Meal.Type.PEPPERONI) + ", " + order1.getBeverage().addBeverage("JUICE", "TWO");
+
 
         Order order2 = new Order();
-        System.out.println("ORDER 2:");
         order2.setClient(client2);
-        System.out.print(order2.getClient().getClientDetails() + ", ");
-        System.out.print(order2.getPizza().addMeal(Meal.Size.SMALL, Meal.Type.HAWAJSKA)+ ", ");
-        System.out.print(order2.getPizza().addMeal(Meal.Size.MEDIUM, Meal.Type.CAPRICIOSA)+ ", ");
-        System.out.println(order2.getBeverage().addBeverage("COKE", "ONE\n"));
+        String orderTwo = "ORDER 2: " + order2.getClient().getClientDetails() + ", " + order2.getPizza().addMeal
+        (Meal.Size.SMALL, Meal.Type.HAWAJSKA) + ", " + order2.getPizza().addMeal(Meal.Size.MEDIUM, Meal.Type.CAPRICIOSA) +
+        ", " + order2.getBeverage().addBeverage("COKE", "ONE");
+
 
         Order order3 = new Order();
-        System.out.println("ORDER 3:");
         order3.setClient(client3);
-        System.out.print(order3.getClient().getClientDetails() + ", ");
-        System.out.print(order3.getHamburger().addHamburger("CLASSIC HAMBURGER" + ", "));
-        System.out.println(order3.getBeverage().addBeverage("BEER", "ONE"));
+        String orderThree = "ORDER 3: " + order3.getClient().getClientDetails() + ", " + order3.getHamburger().addHamburger
+        ("CLASSIC HAMBURGER") + ", " + order3.getBeverage().addBeverage("BEER", "ONE");
+
+        List<String> orderList = new ArrayList<>();
+        orderList.add(orderOne);
+        orderList.add(orderTwo);
+        orderList.add(orderThree);
+
+        System.out.println(orderList.get(0));
+        System.out.println(orderList.get(1));
+        System.out.println(orderList.get(2));
 
     }
 }
