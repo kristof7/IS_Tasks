8d6f8b8717c0109afc09a1b0f8cee48deb6cf352
dosiaczek-tech <59321899+dosiaczek-tech@users.noreply.github.com>
2020-02-28 21:59:42 +0100

    Update DrinkType.java
    "Zmiana ze Strng na String"

--- Ex3/src/main/java/infoshareacademy/Menu/DrinkType.java
+++ Ex3/src/main/java/infoshareacademy/Menu/DrinkType.java
@@ -6,7 +6,7 @@ public enum DrinkType implements ToJson {
     BEER;
 
     @Override
-    public Strng toJson() {
+    public String toJson() {
         return "\"" + this.toString() + "\"";
     }
 }
