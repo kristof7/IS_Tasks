2b9686c4d4ef1a913020f846b88ad2f85fc08b6a
Dominik Rynkiewicz <dominik.rynkiewicz@gmail.com>
2020-05-04 12:01:23 +0200

    Ex 6 - final ver. 1.1 - added jboss-web.xml file

--- Ex6/src/main/java/com/isa/bookcase/servlet/FindBook.java
+++ Ex6/src/main/java/com/isa/bookcase/servlet/FindBook.java
@@ -18,6 +18,7 @@ public class FindBook extends HttpServlet {
 
     @Inject
     private BookService bookService;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -27,48 +28,48 @@ public class FindBook extends HttpServlet {
 
         if (authorParam == null || authorParam.isEmpty()) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        List<Book> booksByAuthor = bookService.getBooksListByAuthor(authorParam);
-        Book bookByAuthor = bookService.getBookByAuthor(authorParam);
-
-        resp.setContentType("text/html;charset=UTF-8");
-        printWriter.println("<!DOCTYPE html>");
-        printWriter.println("<html>");
-        printWriter.println("<body>");
-
-        if (bookByAuthor != null) {
-            for (Book bookFromList : booksByAuthor) {
-                printWriter.println(bookFromList + "<br>");
-                printWriter.println("</body>");
-                printWriter.println("</html>");
-            }
         } else {
-            printWriter.println("Book of this author doesn't exist<br>");
+
+            List<Book> booksByAuthor = bookService.getBooksListByAuthor(authorParam);
+            Book bookByAuthor = bookService.getBookByAuthor(authorParam);
+
+            resp.setContentType("text/html;charset=UTF-8");
+            printWriter.println("<!DOCTYPE html>");
+            printWriter.println("<html>");
+            printWriter.println("<body>");
+
+            if (bookByAuthor != null) {
+                for (Book bookFromList : booksByAuthor) {
+                    printWriter.println(bookFromList + "<br>");
+                    printWriter.println("</body>");
+                    printWriter.println("</html>");
+                }
+            } else {
+                printWriter.println("Book of this author doesn't exist<br>");
+            }
         }
 
         if (titleParam == null || titleParam.isEmpty()) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        List<Book> booksByTitle = bookService.getBooksListByTitle(titleParam);
-        Book bookByTitle = bookService.getBookByTitle(titleParam);
-
-        printWriter.println("<!DOCTYPE html>");
-        printWriter.println("<html>");
-        printWriter.println("<body>");
-
-        if (bookByTitle != null) {
-            for (Book bookFromList : booksByTitle) {
-                printWriter.println(bookFromList + "<br>");
-
-                printWriter.println("</body>");
-                printWriter.println("</html>");
-            }
         } else {
-            printWriter.println("Book with this title doesn't exist<br>");
+
+            List<Book> booksByTitle = bookService.getBooksListByTitle(titleParam);
+            Book bookByTitle = bookService.getBookByTitle(titleParam);
+
+            printWriter.println("<!DOCTYPE html>");
+            printWriter.println("<html>");
+            printWriter.println("<body>");
+
+            if (bookByTitle != null) {
+                for (Book bookFromList : booksByTitle) {
+                    printWriter.println(bookFromList + "<br>");
+
+                    printWriter.println("</body>");
+                    printWriter.println("</html>");
+                }
+            } else {
+                printWriter.println("Book with this title doesn't exist<br>");
+            }
         }
     }
 }
