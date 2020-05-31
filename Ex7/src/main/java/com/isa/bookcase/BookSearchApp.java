ff71fb487ac15628bbad1a58af1b56aea594ded2
Katarzyna <klagocka@gmail.com>
2020-05-31 20:56:16 +0200

    Ex7 homework done added

--- Ex7/src/main/java/com/isa/bookcase/BookSearchApp.java
+++ Ex7/src/main/java/com/isa/bookcase/BookSearchApp.java
@@ -1,12 +1,6 @@
 package com.isa.bookcase;
 
-import com.isa.bookcase.domain.Book;
-import com.isa.bookcase.domain.Category;
 import com.isa.bookcase.service.BookService;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public class BookSearchApp {
 
@@ -14,54 +8,23 @@ public class BookSearchApp {
 
         BookService bookService = new BookService();
 
-        //  1. znajdź tylko książki dla dzieci
-        List<Book> booksForKids; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nKsiążki dla dzieci:");
-        // TODO wypisz wyniki na konsolę
-
-        // 2. znajdź książki, których autor zaczyna się na literę "J"
-        List<Book> booksAuthorStartsWithChar; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nKsiążki, których autor zaczyna sie na litere 'J':");
-        // TODO wypisz wyniki na konsolę
-
-        // 3. znajdź tytuły książek, które zawiera frazę "ci"
-        List<String> bookTitlesWithFragment; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nTytuły książek zawierające frazę 'ci':");
-        // TODO wypisz wyniki na konsolę
-
-        // 4. oblicz ile stron mają wszystkie książki razem
-        Integer sumOfPages; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nWszystkie książki mają razem stron:");
-        // TODO wypisz wynik na konsolę
-
-        // 5. znajdź 3 najkrótsze książki
-        List<Book> threeShortestBooks; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nTrzy najkrótsze książki (tytuł + liczba stron):");
-        // TODO wypisz wyniki na konsolę
-
-        // 6. znajdź tytuły 3 książek, które mają największą liczbę stron
-        List<String> titlesOfThreeLongestBooks; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nTytuły trzech najdłuższych książek:");
-        // TODO wypisz wyniki na konsolę
-
-        // 7. znajdź książkę o najdłuższym tytule
-        String bookWithLongestTitle; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nKsiążka o najdłuzszym tytule:");
-        // TODO wypisz wynik na konsolę
-
-        // 8. wypisz książki (tytuły  i liczbę stron) posortowane wg rosnącej liczby stron
-        List<Book> booksSortedByPagesAsc; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nTytuły książek posortowane wg rosnącej liczby stron:");
-        // TODO wypisz wyniki na konsolę
-
-        // 9. podziel książki wg gatunku
-        Map<Category, List<Book>> booksByCategory; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nKsiążki wg gatunków:");
-        // TODO wypisz wyniki na konsolę
-
-        // 10. znajdź najdłuższą książkę w każdym gatunku
-        Map<Category, Optional<Book>> longestBookInEachCategory; // TODO dodaj odpowiednią metodę w klasie BookService
-        System.out.println("\nNajdłuższa książka w każdym gatunku:");
-        // TODO wypisz wyniki na konsolę
-    }
-}
+        System.out.println("Ksiazki dla dzieci"+bookService.booksForKids());
+        System.out.println();
+        System.out.println("Ksiazki ,których autor zaczyna sie na litere J"+bookService.booksAuthorStartsWithChar());
+        System.out.println();
+        System.out.println("Książki ,które zawierają fraze ci"+bookService.booksTitleWithFragment());
+        System.out.println();
+        System.out.println("Wszystkie ksiazki maja razem stron"+bookService.sumOfPages());
+        System.out.println();
+        System.out.println("Trzy najkrótsze ksiazki"+bookService.threeShortestBooks());
+        System.out.println();
+        System.out.println("Tytuly trzech najdluzszych ksiazek"+bookService.titlesOfThreeLongestBooks());
+        System.out.println();
+        System.out.println("Ksiazka o naj dluzszym tytule"+bookService.bookWithLongestTitle());
+        System.out.println();
+        System.out.println("Tytuly książek posortowane wedlug ilosci stron"+bookService.booksSortedByPagesAsc());
+        System.out.println();
+        System.out.println("Ksiazki wedlug gatunkow" +bookService.booksByCategory());
+        System.out.println();
+        System.out.println("nNajdłuższa książka w każdym gatunku:"+bookService.longestBookInEachCategory());
+    }}
--- Ex7/src/main/java/com/isa/bookcase/service/BookService.java
+++ Ex7/src/main/java/com/isa/bookcase/service/BookService.java
@@ -1,12 +1,90 @@
 package com.isa.bookcase.service;
 
 import com.isa.bookcase.domain.Book;
+import com.isa.bookcase.domain.Category;
 import com.isa.bookcase.repository.Books;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class BookService {
 
     private final List<Book> books = new Books().getBooks();
 
+
+    public List<Book> booksForKids() {
+        return books.stream().filter(book -> book.isForKids())
+                .collect(Collectors.toList());
+    }
+
+    public List<Book> booksAuthorStartsWithChar() {
+        return  books.stream().filter(book ->  book.getAuthor().startsWith("J"))
+            .collect(Collectors.toList());
+
+        }
+    public List<Book> booksTitleWithFragment() {
+        return  books.
+                stream().filter(book ->  book.getTitle().contains("ci"))
+                .collect(Collectors.toList());
+
+    }
+     public Integer sumOfPages() {
+            return books.stream()
+            .map(book ->book.getPages())
+                    .reduce(0, Integer::sum);
+
+
+    }
+    public  List<Book> threeShortestBooks(){
+       return books.stream()
+                .sorted(Comparator.comparing(Book::getPages))
+               .limit(3)
+                .collect(Collectors.toList());}
+
+   public List<String> titlesOfThreeLongestBooks(){
+      return   books.stream()
+              .sorted(Comparator.comparingInt(Book::getPages).reversed())
+              .limit(3)
+              .map(Book::getTitle)
+              .collect(Collectors.toList());
+    }
+   public String bookWithLongestTitle(){
+    return books.stream(). max(Comparator.comparingInt(a ->
+               a.getTitle().length()))
+               .map(Book::getTitle)
+               .orElse(""); //defaulting to 0
+    }
+         public List<Book> booksSortedByPagesAsc(){
+          return  books.stream().sorted(Comparator.comparing(Book::getPages))
+          .collect(Collectors.toList());
 }
+        public Map<Category, List<Book>> booksByCategory(){
+        return books.stream().collect(Collectors.groupingBy(Book::getCategory));
+
+}
+
+  public   Map<Category, Optional<Book>> longestBookInEachCategory(){
+      Comparator<Book> pagesComparator = (u1, u2) -> u1.getPages() - u2.getPages();
+      return books.stream().map(book->book.getCategory()).distinct().collect(Collectors.toMap(category -> category ,category->books.stream()
+              .filter(book->book.getCategory()==category).max(pagesComparator)));
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
