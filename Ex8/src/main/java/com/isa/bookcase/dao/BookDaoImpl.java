374748b0d9c867e1357d43d3f96eba3d8dea819f
AniaCzarnecka <anna.m.czarnecka@gmail.com>
2020-06-25 19:18:18 +0200

    Mock object added

---
+++ Ex8/src/main/java/com/isa/bookcase/dao/BookDaoImpl.java
@@ -0,0 +1,13 @@
+package com.isa.bookcase.dao;
+
+import com.isa.bookcase.entity.Book;
+
+import java.util.List;
+
+public class BookDaoImpl implements BookDao{
+
+    @Override
+    public List<Book> findAllBooks() {
+        return null;
+    }
+}
--- Ex8/src/test/java/com/isa/bookcase/service/BookServiceTest.java
+++ Ex8/src/test/java/com/isa/bookcase/service/BookServiceTest.java
@@ -1,10 +1,29 @@
 package com.isa.bookcase.service;
 
+import com.isa.bookcase.dao.BookDao;
+import com.isa.bookcase.dao.BookDaoImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
+
+    @Mock
+    BookDaoImpl bookDao;
+
+    BookService bookService;
+
+    @BeforeEach
+    public void setUp(){
+        bookService = new BookService();
+    }
+
+
 
     @Test
     public void dummyTest() {
