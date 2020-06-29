7a16d8f07d40f98101055350099e613f2883f0f2
Dominik Rynkiewicz <dominik.rynkiewicz@gmail.com>
2020-06-29 21:12:36 +0200

    Ex 8 - Logger task

--- Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
+++ Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
@@ -1,15 +1,27 @@
 package com.isa.bookcase.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
 
+
+
 public class FileUtils {
+
+    private static Logger LOGGER = LogManager.getLogger(FileUtils.class.getName());
 
     public static void createFile(String pathName) {
         File file = new File(pathName);
         try {
             file.createNewFile();
+            LOGGER.info("New file created");
         } catch (Exception e) {
-            // TODO: log exception message
+            LOGGER.error("Something went wrong");
         }
+    }
+
+    public static void main(String[] args) {
+        FileUtils.createFile("");
     }
 }
