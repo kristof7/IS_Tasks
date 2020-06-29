d9e159a9a957dda90c614893c5a4390d2f3ceb7e
dzaorski <dawid.zaorski@gmail.com>
2020-06-29 21:59:21 +0200

    Ex8: add logs in console and in json file

--- Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
+++ Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
@@ -1,15 +1,33 @@
 package com.isa.bookcase.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
 
 public class FileUtils {
 
-    public static void createFile(String pathName) {
+    private static Logger LOGGER;
+    static {
+        String path = FileUtils.class.getClassLoader()
+                .getResource("log4j2.xml")
+                .getFile();
+        System.setProperty("log4j.configurationFile",  path);
+        LOGGER = LogManager.getLogger(FileUtils.class.getName());
+    }
+
+    public static void createFile(String pathName) throws IOException {
         File file = new File(pathName);
         try {
             file.createNewFile();
         } catch (Exception e) {
             // TODO: log exception message
+            LOGGER.error("Creating file failed!");
+
+            Logger logger = LogManager.getLogger("CONSOLE_JSON_APPENDER");
+            logger.error("Creating file failed!");
         }
     }
 }
--- Ex8/src/test/java/com/isa/bookcase/utils/FileUtilsTest.java
+++ Ex8/src/test/java/com/isa/bookcase/utils/FileUtilsTest.java
@@ -2,10 +2,12 @@ package com.isa.bookcase.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 public class FileUtilsTest {
 
     @Test
-    public void createFileShouldLogException() {
+    public void createFileShouldLogException() throws IOException {
         FileUtils.createFile("");
     }
 }
