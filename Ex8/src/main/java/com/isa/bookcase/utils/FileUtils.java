7d31dc1846932f879ee24c1de14c62797b3ced75
AdamSkretkowicz <adam.skretkowicz@gmail.com>
2020-06-30 22:03:17 +0200

    Ex8 - ver0.2 - loggowanie w aplikacji

--- Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
+++ Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
@@ -1,15 +1,21 @@
 package com.isa.bookcase.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.ErrorHandler;
+
 import java.io.File;
 
 public class FileUtils {
+
+    private static final Logger LOGGER = LogManager.getLogger(ErrorHandler.class.getName());
 
     public static void createFile(String pathName) {
         File file = new File(pathName);
         try {
             file.createNewFile();
         } catch (Exception e) {
-            // TODO: log exception message
+            LOGGER.error("Method createFile error : " + e);
         }
     }
 }
