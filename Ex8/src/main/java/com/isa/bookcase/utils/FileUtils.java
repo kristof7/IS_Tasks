0f33dad90530e9a3f0c20172cac2b3d955779cd8
Jarek Szczepanik <ten.ancymon@gmail.com>
2020-06-30 03:12:58 +0200

    Loggers finished

--- Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
+++ Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
@@ -1,15 +1,23 @@
 package com.isa.bookcase.utils;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.ErrorHandler;
+
 import java.io.File;
 
+
 public class FileUtils {
+
+    private static Logger LOGGER = LogManager.getLogger(ErrorHandler.class.getName());
 
     public static void createFile(String pathName) {
         File file = new File(pathName);
         try {
             file.createNewFile();
         } catch (Exception e) {
-            // TODO: log exception message
+            LOGGER.error("bledna sciezka", e);
         }
     }
 }
