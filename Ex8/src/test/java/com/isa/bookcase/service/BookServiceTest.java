5bbdbcd5faa03fcb84b64a80bed45396d5ec32b8
DorotaMiler <dorota.miler@outlook.com>
2020-06-30 23:56:41 +0200

    last changes

--- Ex8/src/test/java/com/isa/bookcase/service/BookServiceTest.java
+++ Ex8/src/test/java/com/isa/bookcase/service/BookServiceTest.java
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static com.isa.bookcase.entity.Category.*;
@@ -20,7 +21,6 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
-    //    private static final Logger logger = Logger.getLogger(BookServiceTest.class.getName());
     List<Book> books;
 
     @Mock
@@ -43,6 +43,8 @@ public class BookServiceTest {
                 new Book("Brett D. McLaughlin", "Head First Object-Oriented Analysis Design", BIOGRAPHY, 78, true),
                 new Book("Scott Oaks", "Java Performance: The Definite Guide", COMIC, 143, true),
                 new Book("Robert Cecil Martin", "Clean Code – A Handbook of Agile Software Craftsmanship", HISTORICAL, 190, false));
+
+        when(bookDao.findAllBooks()).thenReturn(books);
     }
 
     @AfterEach
@@ -50,81 +52,9 @@ public class BookServiceTest {
         books = null;
     }
 
-//    List<Book> inputList;
-//    Book book;
-
-//    @BeforeEach
-//    public void beforeEachLogger() {
-//        logger.info("This is @BeforeEach method");
-
-//    public void mockitoSetUp() {
-//        bookDao = mock(BookDao.class);
-//        bookService = new BookService(bookDao);
-//    }
-//
-//    inputList=new LinkedList<Book>();
-////            public Book(String author, String title, Category category, int pages, boolean forKids) {
-////            this.author = author;
-//            this.title = title;
-//            this.category = category;
-//            this.pages = pages;
-////            this.forKids = forKids;
-//        inputList.add(new
-//
-
-
-//    @AfterEach
-//    public void afterEach() {
-//        logger.info("This is @AfterEach method");
-//    }
-//
-//    @BeforeAll
-//    public static void beforeAll() {
-//        logger.info("This is @BeforeEach method");
-//    }
-//
-//    @AfterAll
-//    public static void afterAll() {
-//        logger.info("This is @AfterAll method");
-//    }
-///-------------------------------------------------------
-
-//    @Test
-//    @DisplayName("Check of what mock returns")
-//    public void returnFromMock() {
-//        List<Book> allBooksResult = bookService.findAllBooks();
-//        System.out.println(allBooksResult);
-//    }
-
-
-//    @Test
-//    @DisplayName("Checks sorting")
-//    public void testIfLongestBooksSortedDescending() {
-//
-//        ByNumberOfPages() {
-//            // given
-//            List<Book> =new ArrayList<>();
-//            // when
-//
-//            // then
-//        }
-//    }
-
-//    @Test
-//    @DisplayName("Checks if books from:'for kids' caegory filtered properly")
-//    public void testIfBooksForKidsFiltered() {
-//    }
-
-
-    //        List<Book> booksResult = bookService.findBooksForKids();
-//        System.out.println(booksResult);
     @Test
     @DisplayName("Checks if method findAllBooks successfully retrieves all books from the list")
     public void testIfAllBooksListed() {
-        // given
-        when(bookDao.findAllBooks()).thenReturn(books);
-//        System.out.println(bookDao.findAllBooks());
-
         // when
         List<Book> result = bookService.findAllBooks();
 
@@ -136,44 +66,33 @@ public class BookServiceTest {
 
     @Test
     @DisplayName("Checks if books from:'for kids' category filtered properly")
-    void testIfFindBooksForKids() {
-        // given
-        when(bookDao.findAllBooks()).thenReturn(books);
-
+    void testIfFoundBooksForKids() {
         // when
         List<Book> result = bookService.findBooksForKids();
-        System.out.println(result);
 
         // then
         assertThat(result)
-                .hasSize(2)
+                .hasSize(4)
                 .isNotSameAs(books)
-                .contains(books.get(1));
+                .contains(books.get(3));
     }
 
     @Test
     @DisplayName("Checks if author's name with prefix retrieved")
-    void testIfFindBooksAuthorStartsWith() {
-        // given
-        when(bookDao.findAllBooks()).thenReturn(books);
-
+    void testIfFoundBooksAuthorStartsWith() {
         // when
         List<Book> result = bookService.findBooksAuthorStartsWith("S");
-//        System.out.println(result);
 
         // then
         assertThat(result)
                 .hasSize(2)
                 .isNotSameAs(books)
-                .contains(books.get(0),books.get(9));
+                .contains(books.get(0), books.get(9));
     }
 
     @Test
     @DisplayName("Checks if books' retrieved with titles that contain indicated fragment")
-    void testIfFindBooksTitleContains() {
-        // given
-        when(bookDao.findAllBooks()).thenReturn(books);
-
+    void testIfFoundBooksTitleContains() {
         // when
         List<Book> result = bookService.findBooksTitleContains("Java");
 
@@ -181,133 +100,45 @@ public class BookServiceTest {
         assertThat(result)
                 .hasSize(8)
                 .isNotSameAs(books)
-                .contains(books.get(1),books.get(2), books.get(3),
+                .contains(books.get(1), books.get(2), books.get(3),
                         books.get(4), books.get(5), books.get(7), books.get(2));
     }
 
     @Test
-    void findLongestBooks() {
-        // given
-        when(bookDao.findAllBooks()).thenReturn(books);
-
+    @DisplayName("Checks if longest books retrieved")
+    void testIfFoundLongestBooks() {
         // when
         List<Book> result = bookService.findLongestBooks(220);
 
         // then
         assertThat(result)
                 .isNotSameAs(books)
-                .contains(books.get(2),books.get(6))
+                .contains(books.get(2), books.get(6))
                 .doesNotContain(new Book("Herbert Schildt", "Java: A Beginner’s Guide", STORY, 76, true));
-
     }
 
     @Test
-    void bookWithLongestTitle() {
-        // given
-
+    @DisplayName("Checks if book with the longest title retrieved")
+    void testIfBookWithLongestTitle() {
         // when
+        String result = bookService.bookWithLongestTitle();
 
         // then
-
+        assertThat(result)
+                .isEqualTo("Test-Driven: TDD and Acceptance TDD for Java Developers")
+                .isNotEqualTo("Clean Code – A Handbook of Agile Software Craftsmanship");
     }
 
     @Test
-    void sortBooksByPagesAsc() {
-        // given
-
+    @DisplayName("Checks if method sorts books considering nr of pages in ascending order")
+    void testIfSortsBooksByPagesAsc() {
         // when
+        List<Book> result = bookService.sortBooksByPagesAsc();
 
         // then
-
-    }
-
-    @Test
-    void groupBooksByCategory() {
-        // given
-
-        // when
-
-        // then
-
+        assertThat(result)
+                .isNotSameAs(books)
+                .hasSameSizeAs(books)
+                .isSortedAccordingTo(Comparator.comparing(Book::getPages));
     }
 }
-//
-//    @Test
-//    @DisplayName("Checks if author from the list of books starts with given prefix")
-//    testIfBookAuthorStartsWithGivenPrefix() {
-//        // given
-//
-//
-//        // when
-//        // then
-//        assertThat(book.getAuthor()).isNotNull
-//                .startsWith("Ko");
-//    }
-
-
-//    public class MapSorterTest {
-//        private static final Logger logger = Logger.getLogger(MapSorterTest.class.getName());
-//        LinkedHashMap<Integer, String> inputMap;
-//
-//        @BeforeEach
-//        public void beforeEach() {
-//            logger.info("This is @BeforeEach method");
-//            inputMap = new LinkedHashMap<>();
-//            inputMap.put(1, "Frodo");
-//            inputMap.put(4, "Gandalf");
-//            inputMap.put(3, "Legolas");
-//            inputMap.put(7, "Boromir");
-//            inputMap.put(2, "Gimli");
-//            inputMap.put(6, "Alateja");
-//            inputMap.put(5, "Peragrin");
-//        }
-//
-//        @AfterEach
-//        public void afterEach() {
-//            inputMap = null;
-//        }
-//
-//        @Test
-//        @DisplayName("Checks if sorting by value works accordingly")
-//        public void testIfSortsByValue() {
-//            // given
-//            LinkedHashMap<Integer, String> expectedSortedMapByValue = new LinkedHashMap<>();
-//            expectedSortedMapByValue.put(6, "Alateja");
-//            expectedSortedMapByValue.put(7, "Boromir");
-//            expectedSortedMapByValue.put(1, "Frodo");
-//            expectedSortedMapByValue.put(4, "Gandalf");
-//            expectedSortedMapByValue.put(2, "Gimli");
-//            expectedSortedMapByValue.put(3, "Legolas");
-//            expectedSortedMapByValue.put(5, "Peragrin");
-//            // when
-//            Map<Integer, String> result = MapSorter.sortByValue(inputMap);
-//
-//            // then
-//            assertThat(result).containsExactlyEntriesOf(expectedSortedMapByValue)
-//                    .isEqualTo(expectedSortedMapByValue)
-//                    .isNotSameAs(inputMap)
-//                    .hasSameSizeAs(inputMap);
-//        }
-//
-//        @Test
-//        @DisplayName("Checks if sorting by key works accordingly")
-//        public void testIfSortsByKey() {
-//            // given
-//            LinkedHashMap<Integer, String> expectedSortedMapByKey = new LinkedHashMap<>();
-//            expectedSortedMapByKey.put(1, "Frodo");
-//            expectedSortedMapByKey.put(2, "Gimli");
-//            expectedSortedMapByKey.put(3, "Legolas");
-//            expectedSortedMapByKey.put(4, "Gandalf");
-//            expectedSortedMapByKey.put(5, "Peragrin");
-//            expectedSortedMapByKey.put(6, "Alateja");
-//            expectedSortedMapByKey.put(7, "Boromir");
-//
-//            // when
-//            Map<Integer, String> result = MapSorter.sortByKey(inputMap);
-//
-//            // then
-//            assertThat(result).containsExactlyEntriesOf(expectedSortedMapByKey)
-//                    .isEqualTo(expectedSortedMapByKey)
-//                    .isNotSameAs(inputMap)
-//                    .hasSameSizeAs(inputMap);
-//        }
