d05804fe37814a4284d11b54eef7bc4060240031
Katarzyna <klagocka@gmail.com>
2020-07-03 17:47:21 +0200

    Logger homework added

--- Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
+++ Ex8/src/main/java/com/isa/bookcase/utils/FileUtils.java
@@ -1,15 +1,32 @@
 package com.isa.bookcase.utils;
 
 import java.io.File;
-
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Objects;
 public class FileUtils {
+    private static final Logger LOGGER;
+    static {
+
+        String path=FileUtils.class.getClassLoader()
+                .getResource("log4j2.xml")
+                .getFile();
+        System.setProperty("log4j.configurationFile",path);
+        LOGGER=LogManager.getLogger(FileUtils.class.getName());
+
+    }
+
+
 
     public static void createFile(String pathName) {
         File file = new File(pathName);
         try {
             file.createNewFile();
+            LOGGER.log(Level.getLevel("INFO"),"New File");
         } catch (Exception e) {
-            // TODO: log exception message
+            LOGGER.log(Level.getLevel("INFO"),"error");
+
         }
     }
 }
--- Ex8/src/test/java/com/isa/bookcase/utils/FileUtilsTest.java
+++ Ex8/src/test/java/com/isa/bookcase/utils/FileUtilsTest.java
@@ -2,10 +2,16 @@ package com.isa.bookcase.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 public class FileUtilsTest {
 
     @Test
     public void createFileShouldLogException() {
         FileUtils.createFile("");
+    }
+    @Test
+    public void ifcreatedFile()throws IOException {
+        FileUtils.createFile("logs4j2.txt");
     }
 }
